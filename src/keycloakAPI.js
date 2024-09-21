0249d5f41937ea41ec46971e6d54b25cd034fbb9
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-21 01:55:52 -0700

    Completely rewrote because the old version wouldn't have worked

--- src/keycloakAPI.js
+++ src/keycloakAPI.js
@@ -1,9 +1,20 @@
 const mTLSAgent = require("./mTLSAgent");
-const axios = require("axios").default;
+const { Issuer } = require("openid-client");
+const { custom } = require("openid-client");
 
-const keycloakAPI = axios.create({
-    baseURL: process.env.ISSUER_BASE_URL,
-    httpsAgent: mTLSAgent,
+const keycloak = await Issuer.discover(process.env.ISSUER_BASE_URL);
+
+const keycloakAPI = new keycloak.Client({
+    client_id: process.env.CLIENT_ID,
+    token_endpoint_auth_method: "tls_client_auth",
+    token_endpoint_auth_signing_alg: "EdDSA",
+    id_token_signed_response_alg: "EdDSA",
+    authorization_signed_response_alg: "EdDSA",
+    tls_client_certificate_bound_access_tokens: true,
 })
+
+keycloakAPI[custom.http_options] = function(url, options) {
+    return { agent: mTLSAgent };
+}
 
 module.exports = keycloakAPI;
