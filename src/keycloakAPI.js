be8f6a2cbd18eb056617dc3fa02cb94de53b4c85
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-21 20:29:15 -0700

    asyncified - who knows if this'll work

--- src/keycloakAPI.js
+++ src/keycloakAPI.js
@@ -1,6 +1,7 @@
 const mTLSAgent = require("./mTLSAgent");
 const { Issuer } = require("openid-client");
 const { custom } = require("openid-client");
+const { readFile } = require("fs/promises");
 
 const keycloak = await Issuer.discover(process.env.ISSUER_BASE_URL);
 
@@ -13,8 +14,11 @@ const keycloakAPI = new keycloak.Client({
     tls_client_certificate_bound_access_tokens: true,
 })
 
-keycloakAPI[custom.http_options] = function(url, options) {
-    return { agent: mTLSAgent };
+keycloakAPI[custom.http_options] = async function(url, options) {
+    return { 
+        key: await readFile(process.env.CLIENT_KEY_PATH, "utf8"),
+        cert: await readFile(process.env.CLIENT_CERT_PATH, "utf8"),
+    };
 }
 
 module.exports = keycloakAPI;
