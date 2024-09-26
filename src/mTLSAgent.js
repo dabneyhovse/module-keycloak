c303bf50d905a9e24901af827409be3eada945df
Melissa Foster <melissaemilyfoster@gmail.com>
2024-09-25 17:16:50 -0700

    WTF

--- src/mTLSAgent.js
+++ src/mTLSAgent.js
@@ -1,9 +1,9 @@
 import { Agent } from "https";
-import { readFile } from "fs/promises";
+import { readFileSync } from "fs";
 
 const mTLSAgent = new Agent({
-  key: await readFile(process.env.CLIENT_KEY_PATH, { encoding: "utf8"}),
-  cert: await readFile(process.env.CLIENT_CERT_PATH, { encoding: "utf8"}),
+  key: readFileSync(process.env.CLIENT_KEY_PATH, "utf8"),
+  cert: readFileSync(process.env.CLIENT_CERT_PATH, "utf8"),
 });
 
 export default mTLSAgent;
